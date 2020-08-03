8e167a52880a44315a286421e76279e38dc85214
Evan Mackay <evanmackay@Evans-MBP.hsd1.ut.comcast.net>
2020-08-03 16:09:21 -0600

    added code to generate random passwords. Still need to make website responsive.

---
+++ script.js
@@ -0,0 +1,64 @@
+
+// Variables defined to generate random password
+let lowerCaseLtrs = "abcdefghijklmnopqrstuvwxyz"
+let lowerCaseLtrsArr = lowerCaseLtrs.split("")
+let upperCaseLtrs = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+let upperCaseLtrsArr = upperCaseLtrs.split("")
+let numbers = "0123456789"
+let numbersArr = numbers.split("")
+let symbols =  "!#$%&'()*+,-./:;<=>?@[\]^_`{|}~"
+let symbolsArr = symbols.split("")
+// alert informing user to select random password
+window.onload = alert("Click the Genereate Password button to generate a new password!")
+// Password generating function
+function generatePass (){
+  let allEl = []
+  let completedPass = ""
+// initial propmt for user to select character length
+  let passwordPrompt = prompt("Select a character length between 8 and 128 characters.")
+// pulls from the array values to select which random characters will be used.
+  if (passwordPrompt < 8 || passwordPrompt > 128) {
+    alert("Password must be within 8 or 128 characters.")
+  } else {
+    if(confirm("Would you like to add uppercase letters?")) {
+      Array.prototype.push.apply(allEl, upperCaseLtrsArr)
+    }
+    if(confirm("Would you like to add lowercase letters?")) {
+      Array.prototype.push.apply(allEl, lowerCaseLtrsArr)
+    }
+    if(confirm("Would you like to add numbers?")) {
+      Array.prototype.push.apply(allEl, numbersArr)
+    }
+    if(confirm("Would you like to add symbols?")) {
+      Array.prototype.push.apply(allEl, symbolsArr)
+    }
+    // if user doesn't select any criteria, they will be alerted to select at least one criteria
+    if(allEl.length === 0) {
+      alert("You must select at least one type of character to generate a password. Press the Generate Password button to try again.")
+      // runs a loop that pieces together the selected elements and creates a randomly generated password
+    } else {
+      for(let i = 0; i < passwordPrompt; i++) {
+        let randomPass = Math.floor(Math.random() * allEl.length);
+        completedPass += allEl[randomPass]
+      }
+    }
+  }
+  // grabs the resulting password and displays the completed password in the text area.
+  document.getElementById("password").innerHTML = completedPass
+  return completedPass
+}
+
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePass();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
